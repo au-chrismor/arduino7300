24c85afbc127f7c39cae463953d9340b181eaf4c
Chris Moran <au-chrismor@users.noreply.github.com>
2024-09-24 13:40:56 +1000

    Pre-allocate for PWM Fan

--- arduino7300.h
+++ arduino7300.h
@@ -10,15 +10,24 @@
 #define RLY_10M        D2
 #define RLY_15M        D3
 #define RLY_30M        D6
-#define RLY_40M        D6
+#define RLY_40M        D7
 #define RLY_80M        D8
-#define TX_PIN         D9
+#define FAN_PIN        D9
+#define TX_PIN         D10
+#define TBD_11         D11
+#define TBD_12         D12
+
 #define SDA_PIN        D4
 #define SCL_PIN        D5
 
 #define BAND_IN        A0
 #define THERM_PIN      A1
 #define CURRENT_PIN    A2
+#define TBD_A3         A3
+#define TBD_A4         A4
+#define TBD_A5         A5
+#define TBD_A6         A6
+#define TBD_A7         A7
 
 #define LCD_ROWS       4
 #define LCD_COLS       20
