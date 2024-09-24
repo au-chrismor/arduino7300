940524408387bc966c8c6af360300dae905367dd
Chris Moran <au-chrismor@users.noreply.github.com>
2024-09-24 10:38:16 +1000

    Create arduino7300.h

---
+++ arduino7300.h
@@ -0,0 +1,32 @@
+// Controller for LPF and LDMOS amplifier
+// Christopher F. Moran, VK2MEI
+//
+
+#ifndef __ARDUINO7300_H_INC__
+
+#include <thermistor.h>
+#include <LiquidCrystal_I2C.h>
+
+#define RLY_10M        D2
+#define RLY_15M        D3
+#define RLY_30M        D4
+#define RLY_40M        D5
+#define RLY_80M        D6
+#define TX_PIN         D7
+
+#define BAND_IN        A0
+#define THERM_PIN      A1
+#define CURRENT_PIN    A2
+
+#define LCD_ROWS       4
+#define LCD_COLS       20
+#define LCD_ADDR       0x3F
+
+#define THERM_RES      10000
+#define THERM_BETA     3950
+#define THERM_SERIES   9980
+
+#define NUM_SAMPLES   3
+
+#define __ARDUINO7300_H_INC__
+#endif
